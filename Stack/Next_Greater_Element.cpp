a728bc354ef9415638f3a651d0a7906348b9eabd
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-02-24 22:50:29 +0530

    Added Next Greater and prev Greater ele

--- Stack/Next_Greater_Element.cpp
+++ Stack/Next_Greater_Element.cpp
@@ -10,29 +10,33 @@ Institution: NIT Meghalaya
 #define rall(v) v.rbegin(), v.rend()
 using namespace std;
 //Time Complexity: O(n)
-void print_previous_greater(vector<int> arr, int n)
+void print_next_greater(vector<int> arr, int n)
 {
     stack<int> st;
-    st.push(arr[0]);
-    cout << "-1 ";
-    for (int i = 1; i < n; i++)
+    st.push(arr[n - 1]);
+    vector<int> res;
+    res.push_back(-1);
+    for (int i = n - 2; i >= 0; i--)
     {
         while (st.empty() == false && st.top() <= arr[i])
             st.pop();
         int temp_res = st.empty() ? -1 : st.top();
-        cout << temp_res << " ";
+        res.push_back(temp_res);
         st.push(arr[i]);
     }
+    for (int i = n - 1; i >= 0; i--)
+        cout << res[i] << " ";
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
+    lli n;
     cin >> n;
     vector<int> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    print_previous_greater(arr, n);
+    print_next_greater(arr, n);
+
     return 0;
 }
